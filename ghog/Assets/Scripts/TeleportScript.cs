6e27fb05afcf86ea89512aee421f808d26975652
Abby Norris <norriabi@sheridancollege.ca>
2022-04-03 01:55:37 -0400

    Added teleporting and shed

---
+++ ghog/Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject player;
+    public bool shed;
+    public GameObject ghostOwner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = teleportTarget.transform.position;
+
+        if (shed) {
+            ghostOwner.SetActive(true);
+        }
+    }
+}
